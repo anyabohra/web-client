6084fc8250ede7e7f4c41f648ece31e0a120e2f6
Cannon Lock <clock@wisc.edu>
2023-11-14 14:38:00 -0600

    Add Coverage information

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  coverageDirectory: ".github/coverage",
+  coverageReporters: [
+    "json-summary"
+  ],
   globals: {
     fetch: global.fetch,
   }
