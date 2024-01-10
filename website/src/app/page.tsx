abf6dd21b9f12b3964df1075189a4e4490ba8824
Cannon Lock <clock@wisc.edu>
2024-01-10 13:15:48 -0600

    Update demo form
    - Use Object Name
    - Discovery URL -> Federation Name
    - Dropdown that defaults to OSDF
    - Make the write out the host name and assume the https://

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,17 +1,28 @@
 'use client'
 
 import Box from "@mui/material/Box";
-import {TextField, Typography} from "@mui/material";
+import {TextField, Typography, Autocomplete, createFilterOptions} from "@mui/material";
 import { Button} from "@mui/material";
 import {Grid} from "@mui/material";
 import {useState} from "react";
 import Client from "@pelicanplatform/web-client";
 
+interface FederationName {
+  label: string,
+  value: string
+}
+
+let federationNames: FederationName[] = [{
+    label: "OSDF",
+    value: "https://osg-htc.org"
+}]
+
+const filter = createFilterOptions<FederationName>();
 
 export default function Home() {
 
-  let [discoveryUrl, setDiscoveryUrl] = useState<string>("https://osg-htc.org");
-  let [discoveryUrlError, setDiscoveryUrlError] = useState<string | undefined>(undefined);
+  let [federationNamespace, setFederationNamespace] = useState<FederationName | undefined>({label: "OSDF", value: "https://osg-htc.org"});
+  let [federationNamespaceError, setFederationNamespaceError] = useState<string | undefined>(undefined);
   let [filePath, setFilePath] = useState<string>("/osgconnect/public/osg/testfile.txt");
   let [filePathError, setFilePathError] = useState<string | undefined>(undefined);
 
@@ -20,18 +31,18 @@ export default function Home() {
   const submit = async () => {
 
     let isValid = true;
-    if(discoveryUrl === "") {
-      setDiscoveryUrlError("Discovery Url is required");
+    if(federationNamespace === undefined) {
+      setFederationNamespaceError("Federation Name is required");
       isValid = false
     }
     if(filePath === "") {
-      setFilePathError("File Path is required");
+      setFilePathError("Object Name is required");
       isValid = false
     }
 
     if(!isValid) return;
 
-    const webClient = new Client(discoveryUrl!);
+    const webClient = new Client(federationNamespace?.value!);
     try {
       await webClient.getFile(filePath)
     } catch (e) {
@@ -39,14 +50,50 @@ export default function Home() {
     }
   }
 
+
   return (
       <Box height={"90vh"}>
         <Grid height={"100%"} justifyContent={"center"} container>
           <Grid item xl={4} md={8} xs={11} display={"flex"}>
             <Box mt={6} mx={"auto"} width={"100%"} display={"flex"} flexDirection={"column"}>
-              <TextField fullWidth onChange={e => {setDiscoveryUrl(e.target.value)}} value={discoveryUrl} id="outlined-basic" label="Discovery Url" variant="outlined" />
               <Box pt={1}>
-                <TextField fullWidth onChange={e => {setFilePath(e.target.value)}} value={filePath} id="outlined-basic" label="File Path" variant="outlined" />
+                <Autocomplete
+                    fullWidth
+                    selectOnFocus
+                    clearOnBlur
+                    handleHomeEndKeys
+                    options={federationNames}
+                    value={federationNamespace}
+                    onChange={(e, newValue) => {
+											if (newValue && federationNames.includes(newValue as FederationName)) {
+                        setFederationNamespace(newValue);
+											} else if(newValue && newValue.label.startsWith("Add")){
+                        setFederationNamespace({
+                          label: newValue.value,
+                          value: newValue.value
+                        })
+                      }
+                    }}
+                    filterOptions={(options, params) => {
+                      const filtered = filter(options, params);
+
+                      const { inputValue } = params;
+                      // Suggest the creation of a new value
+                      const isExisting = options.some((option) => inputValue === option.label);
+                      if (inputValue !== '' && !isExisting) {
+                        filtered.push({
+                          value: inputValue,
+                          label: `Add "${inputValue}"`,
+                        });
+                      }
+                      return filtered;
+                    }}
+                    id={"outlined-basic"}
+                    renderInput={(params) => <TextField {...params} label="Federation Name" />}
+                />
+              </Box>
+              <Box pt={2}>
+                <TextField fullWidth onChange={e => {setFilePath(e.target.value)}} value={filePath} id="outlined-basic" label="Object Name" variant="outlined" />
               </Box>
               { submitError ?
                 <Box>
